3651482919409603a89e37905b2627ad751374f9
brightrealitydotrealestate-source <brightrealitydotrealestate@gmail.com>
2026-04-18 21:41:41 +0530

    Update constants.ts - New Instagram Page Landing Correction

--- constants.ts
+++ constants.ts
@@ -112,7 +112,7 @@ const GoogleIcon = (props: React.SVGProps<SVGSVGElement>) => (
 );
 
 export const SOCIAL_LINKS: SocialLink[] = [
-  { platform: "Instagram", url: "https://www.instagram.com/kushibusy/", icon: Instagram },
+  { platform: "Instagram", url: "https://www.instagram.com/kushibusy.in/", icon: Instagram },
   { platform: "YouTube", url: "https://www.youtube.com/@KUSHIBUSY", icon: Youtube },
   { platform: "Facebook", url: "https://www.facebook.com/people/KushiBusy-Channel/pfbid0VvRTuxSp9ZigtQGDTEzF3WNf7UKTYGieUFtfhe2qxrSeNgVxHudtg8HjwHv3R5T7l/", icon: Facebook },
   { platform: "WhatsApp", url: "https://wa.me/+919840013421", icon: WhatsAppBrandIcon },
